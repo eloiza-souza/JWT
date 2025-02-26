bf77a33d8023cbde420c7bd898333ee20b3892fd
Eloiza <eloiza.souza@zup.com.br>
2025-02-26 16:30:29 -0300

    test: develop tests to the UserService class in the registerUser method

---
+++ src/test/java/com/eloiza/JWT/services/UserServiceTest.java
@@ -0,0 +1,99 @@
+package com.eloiza.JWT.services;
+
+import com.eloiza.JWT.controllers.dtos.AuthUserDto;
+import com.eloiza.JWT.controllers.dtos.RegisterUserDto;
+import com.eloiza.JWT.infra.jwt.JwtTokenProvider;
+import com.eloiza.JWT.models.CustomUserDetails;
+import com.eloiza.JWT.models.Department;
+import com.eloiza.JWT.models.Role;
+import com.eloiza.JWT.models.User;
+import com.eloiza.JWT.repositories.DepartmentRepository;
+import com.eloiza.JWT.repositories.RoleRepository;
+import com.eloiza.JWT.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private DepartmentRepository departmentRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private SecurityContext securityContext;
+
+    @Mock
+    private Authentication authentication;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void registerUser_Success() {
+        RegisterUserDto registerUserDto = new RegisterUserDto();
+        registerUserDto.setName("Test User");
+        registerUserDto.setUsername("testuser");
+        registerUserDto.setEmail("testuser@example.com");
+        registerUserDto.setPassword("password");
+        registerUserDto.setRoles(Set.of("ROLE_USER"));
+        registerUserDto.setDepartment("IT");
+
+        when(userRepository.existsByUsername("testuser")).thenReturn(false);
+        when(bCryptPasswordEncoder.encode("password")).thenReturn("encodedPassword");
+        when(roleRepository.findByName("ROLE_USER")).thenReturn(Optional.empty());
+        when(roleRepository.save(any(Role.class))).thenReturn(new Role("ROLE_USER"));
+        when(departmentRepository.findByName("IT")).thenReturn(Optional.empty());
+        when(departmentRepository.save(any(Department.class))).thenReturn(new Department("IT"));
+
+        userService.registerUser(registerUserDto);
+
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void registerUser_ExistingUser() {
+        RegisterUserDto registerUserDto = new RegisterUserDto();
+        registerUserDto.setUsername("testuser");
+
+        when(userRepository.existsByUsername("testuser")).thenReturn(true);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> userService.registerUser(registerUserDto));
+        assertEquals("Usuário já cadastrado no sistema", exception.getMessage());
+        verify(userRepository, never()).save(any(User.class));
+
+    }
+
+   
+
+}
